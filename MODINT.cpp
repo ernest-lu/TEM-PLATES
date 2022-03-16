b50e13c696df61f98955420588cf72137bcadeb8
Ernest Lu <ernest@berkeley.edu>
2022-03-15 20:27:25 -0700

    great

--- MODINT.cpp
+++ MODINT.cpp
@@ -19,41 +19,23 @@ template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {cou
 const LL MOD = 1e9 + 7;
 struct mint {
     int x;
- 
     mint() : x(0) {}
- 
     template <class T> mint(T value) : x(value % MOD) { if (x < 0) x += MOD; }
- 
     mint & operator+=(const mint &oth) { x += oth.x; if (x >= MOD) x -= MOD; return *this; }
- 
     mint & operator-=(const mint &oth) { x -= oth.x; if (x < 0) x += MOD; return *this; }
- 
     mint & operator*=(const mint &oth) { x = (long long) x * oth.x % MOD; return *this; }
- 
     friend mint operator+(mint l, const mint &r) { return l += r; }
- 
     friend mint operator-(mint l, const mint &r) { return l -= r; }
- 
     friend mint operator*(mint l, const mint &r) { return l *= r; }
- 
     mint & operator--() { if (--x == -1) x = MOD - 1; return *this; }
- 
     mint & operator++() { if (++x == MOD) x = 0; return *this; }
- 
     mint operator--(int) { mint temp = *this; --*this; return temp; }
- 
     mint operator++(int) { mint temp = *this; ++*this; return temp; }
- 
     mint operator-() const { return 0 - *this; }
- 
     mint operator+() const { return *this; }
- 
     friend bool operator==(const mint &l, const mint &r) { return l.x == r.x; }
- 
     friend bool operator!=(const mint &l, const mint &r) { return l.x != r.x; }
- 
     friend ostream & operator<<(ostream &out, const mint &a) { return out << a.x; }
- 
     mint pow(long long e = MOD - 2) const {
         mint ans = 1, b = *this;
         while (e > 0) {
